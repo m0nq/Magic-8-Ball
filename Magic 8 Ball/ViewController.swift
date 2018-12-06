e902ec6588ea8a141c93e0f3c5003cc50f112f0b
Monk Wellington <monk@industriallogic.com>
2018-12-06 14:48:23 -0800

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Monk Wellington on 12/6/18.
+//  Copyright © 2018 Industrial Logic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
