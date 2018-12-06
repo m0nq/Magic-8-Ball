808289f84da1fc642d8fb8d913178275d8db1207
Monk Wellington <monk@industriallogic.com>
2018-12-06 15:20:38 -0800

    Initial commit: Create basic boilerplate and ensure functionality

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray: [String] = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber: Int = 0
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        newBallImage()
     }
 
+    @IBAction func askButtonPressed(_ sender: Any) {
+    }
 
+    private func newBallImage() {
+        imageView.image = UIImage(named: ballArray[Int.random(in: 0...4)])
+    }
 }
 
